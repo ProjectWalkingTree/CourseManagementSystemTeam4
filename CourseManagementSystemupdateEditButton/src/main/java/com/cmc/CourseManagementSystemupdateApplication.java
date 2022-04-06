fbecfc89bc1130db4cc1fd123387eb91e6934420
ProjectWalkingTree <akash.rajput@walkingtree.tech>
2022-04-06 12:07:11 +0530

    edit

---
+++ CourseManagementSystemupdateEditButton/src/main/java/com/cmc/CourseManagementSystemupdateApplication.java
@@ -0,0 +1,18 @@
+package com.cmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+
+public class CourseManagementSystemupdateApplication 
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CourseManagementSystemupdateApplication.class, args);
+	}
+
+}
